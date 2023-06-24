e8e09f52860fc339f1a1eba22e883d1bb8aec7db
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-06-24 13:58:23 +0200

    div to section in Tile changed

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -1,6 +1,6 @@
 import { styled } from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.section`
   background-color: ${({ theme }) => theme.colors.white};
   display: flex;
   flex-direction: column;
