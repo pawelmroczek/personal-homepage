428217042033d506d7fbb7173e3c3395a14efa9d
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-06-24 08:52:42 +0200

    colors to theme added

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,12 @@
 const theme ={
   colors:{
+    white:"#FFFFFF",
     BGLightMode: "#FBFBFE",
     blackText:"#252525",
     lightGrey:"#E5E5E5",
     textSecondary:"#6E7E91",
+    scienceBlue:"#0366D6",
+    diffBGNeutral:"#D1D5DA4D"
   }
 }
 
