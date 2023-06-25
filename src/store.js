1b39878bfe0730586c7f7f8ffe852978d356cd1d
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-06-25 14:13:54 +0200

    store for saga configured

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import portfolioSlice from "./features/Portfolio/portfolioSlice";
+import createSagaMiddleware from "redux-saga";
+import { portfolioSaga } from "./features/Portfolio/portfolioSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     portfolio: portfolioSlice,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(portfolioSaga);
 
 export default store;
