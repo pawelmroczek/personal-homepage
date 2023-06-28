e40a55715fc8148f6fea2ebe111d09f081801787
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-06-28 18:35:35 +0200

    success container added

--- src/features/Portfolio/Projects/Success/index.js
+++ src/features/Portfolio/Projects/Success/index.js
@@ -1,7 +1,24 @@
-const Success = () =>{
-  return (<div>
-    sukces
-  </div>)
-}
+import { useSelector } from "react-redux";
+import { selectProjects } from "../../portfolioSlice";
+import { Container, StyledLink, StyledParagraph, Tile, Title } from "./styled";
 
-export default Success
+const Success = () => {
+  const projects = useSelector(selectProjects);
+
+  return (
+    <Container>
+      {projects.map((project) => (
+        <Tile>
+          <Title>{project.name}</Title>
+          <StyledParagraph>{project.description}</StyledParagraph>
+          <div>
+            Demo: <StyledLink>{project.homepage}</StyledLink> <br />
+            Code: <StyledLink>{project.html_url}</StyledLink>
+          </div>
+        </Tile>
+      ))}
+    </Container>
+  );
+};
+
+export default Success;
---
+++ src/features/Portfolio/Projects/Success/styled.js
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 32px;
+  margin-top: 24px;
+  font-size: 18px;
+  font-family: Inter;
+  line-height: 140%;
+  letter-spacing: 0.9px;
+  color: ${({ theme }) => theme.colors.textSecondary};
+`;
+
+export const Tile = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 56px;
+  box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
+    0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+
+  display: flex;
+  flex-direction:column;
+  justify-content:space-between;
+  
+`;
+
+export const Title = styled.h3`
+  font-size: 24px;
+  font-family: Inter;
+  font-weight: 700;
+  letter-spacing: 1.2px;
+  color: ${({ theme }) => theme.colors.scienceBlue};
+  margin: 0;
+`;
+
+export const StyledParagraph = styled.div`
+  margin: 24px 0;
+`;
+export const StyledLink = styled.a`
+  font-size: 18px;
+  font-family: Inter;
+  line-height: 140%;
+  letter-spacing: 0.9px;
+  color: ${({ theme }) => theme.colors.scienceBlue};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.scienceBlue + "33"};
+`;
